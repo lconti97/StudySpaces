e487bcf967ceedfde2b09400d753dd0607506a5d
lconti97 <lconti97@gmail.com>
2017-10-07 02:12:39 -0400

    refactor index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+
+// var pylons = {lat: 37.229, lng: -80.420};
+navigator.geolocation.getCurrentPosition(alertLocation)
+
+var sampleStudent = {
+  lat: 37.229,
+  long: -80.420,
+  name: "Lucas",
+  id: 13,
+  currentCourse: "CS 2506"
+};
+
+function alertLocation(position) {
+  var currentLocation = {
+    lat: position.coords.latitude,
+    lng: position.coords.longitude
+  };
+
+  var map = createMapCenteredOnLocation(currentLocation);
+  addMarkerToMap(currentLocation, map);
+}
+
+function createMapCenteredOnLocation() {
+  return new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: currentLocation
+  });
+}
+
+function addMarkerToMap(currentLocation, map) {
+  new google.maps.Marker({
+    position: currentLocation,
+    map: map
+  });
+}
